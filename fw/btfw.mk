7339ba12f4d68978e661705b9688a1a7fd73bf7f
Parvathi Hosakote Ramalingappa <parvathix.hosakote.ramalingappa@intel.com>
2014-07-22 11:10:20 +0530

    Bluetooth: Adding FW for Wilkins Peak-1 (B6)
    Category: Device Enablement
    Domain: AOSP-Bluetooth
    Origin: Internal
    Upstream-Candidate: Yes
    
    Version: WP_1345_01_patch_0.0.13.1 (0x0D)
    
    Change-Id: I1dd7412f01ecf8329b87b9521490676b23c26368
    Signed-off-by: Parvathi Hosakote Ramalingappa <parvathix.hosakote.ramalingappa@intel.com>
    Reviewed-on: https://android.intel.com/220213
    Reviewed-by: sys_pccgand <sys_pccgand@intel.com>
    Tested-by: sys_pccgand <sys_pccgand@intel.com>
    Reviewed-by: Debnath, Paritosh <paritosh.debnath@intel.com>
    Reviewed-by: Chittim Munirathnam, MadhusudanX <madhusudanx.chittim.munirathnam@intel.com>

--- fw/btfw.mk
+++ fw/btfw.mk
@@ -2,6 +2,7 @@
 PRODUCT_COPY_FILES += \
         vendor/intel/hardware/bluetooth/fw/370710010002030d00.seq:system/etc/firmware/370710010002030d00.seq \
         vendor/intel/hardware/bluetooth/fw/370710018002030d00.seq:system/etc/firmware/370710018002030d00.seq \
-        vendor/intel/hardware/bluetooth/fw/3707100180012d0d00.seq:system/etc/firmware/3707100180012d0d00.seq
+        vendor/intel/hardware/bluetooth/fw/3707100180012d0d00.seq:system/etc/firmware/3707100180012d0d00.seq \
+        vendor/intel/hardware/bluetooth/fw/3707100100012d0d00.seq:system/etc/firmware/3707100100012d0d00.seq
 
 
