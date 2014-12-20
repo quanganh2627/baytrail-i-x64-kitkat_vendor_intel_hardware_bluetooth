25d7e1f2db60cca327ed27068ee713deaa3b0b10
Navin Kochar <navin.kochar@intel.com>
2014-12-20 15:42:55 +0530

    Bluetooth: Removing redundant BT FW patch files
    Renaming BT FW patch files and removing redundant
    FW patch files for WP1 & WP2.
    
    Change-Id: I61bafc389f47b81ff92d31a1e8321c50d55a66c6
    Signed-off-by: Navin Kochar <navin.kochar@intel.com>
    Reviewed-on: https://android.intel.com/165717
    Reviewed-by: Debnath, Paritosh <paritosh.debnath@intel.com>
    Reviewed-by: Lilja, Ola <ola.lilja@intel.com>

--- fw/btfw.mk
+++ fw/btfw.mk
@@ -1,8 +1,6 @@
 # Firmware patch-file
 PRODUCT_COPY_FILES += \
-        vendor/intel/hardware/bluetooth/fw/370710010002030d36.seq:system/etc/firmware/370710010002030d00.seq \
-        vendor/intel/hardware/bluetooth/fw/370710010002030d36.seq:system/etc/firmware/370710010002030d36.seq \
-	vendor/intel/hardware/bluetooth/fw/370710018002030d36.seq:system/etc/firmware/370710018002030d00.seq \
-	vendor/intel/hardware/bluetooth/fw/370710018002030d36.seq:system/etc/firmware/370710018002030d36.seq
+        vendor/intel/hardware/bluetooth/fw/370710010002030d00.seq:system/etc/firmware/370710010002030d00.seq \
+        vendor/intel/hardware/bluetooth/fw/370710018002030d00.seq:system/etc/firmware/370710018002030d00.seq \
 
 
